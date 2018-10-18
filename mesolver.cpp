97d7e6b6abe807d871fc8023dc35240617a92fdf
zhijie0710 <44274816+zhijie0710@users.noreply.github.com>
2018-10-18 13:52:33 -0600

    Update mesolver.cpp

--- mesolver.cpp
+++ mesolver.cpp
@@ -1,1 +1,12 @@
-#include 
+#include<stdio.h>
+#include <iostream>
+#include "mesolver.h"
+
+
+void RK4(H, rho, dt, *cOps, *coeff) {
+  .......
+}
+
+void lindbladME(H, rho, *cOps, *coeff) {
+  .......
+}
