15782802719401058e5ee83a075ba37999a9fd99
David Rodriguez Perez <davidrp25@gmail.com>
2018-11-09 15:19:05 -0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,48 @@
+// #include <iostream>
+#include "mesolver.h"
+
+using namespace std;
+using namespace arma;
+
+float pulse(float t, vector<float> params) {
+	return(params[0]*sin(PI*t/params[1]));
+}
+
+int main() {
+
+	mesolver me;
+
+	cx_mat rho0, H0, a, ad, rho1, sigmax, sigmaz;
+	vector<cx_mat> cOps, tOps, Ht;
+	float dt, tmax;
+	vector<float> coeff;
+	vector<vector<float>> params;
+	bool isSparse = 0;
+
+	a = zeros<cx_mat>(2,2);
+	ad = zeros<cx_mat>(2,2);
+	rho0 = zeros<cx_mat>(2,2);
+	rho1 = zeros<cx_mat>(2,2);
+	a(0, 1) = 1; ad(1, 0) = 1;
+	rho0(0, 0) = 1; rho1(1, 1) = 1;
+	sigmax = a + ad;
+	sigmaz = rho0 - rho1;
+	H0 = sigmaz;
+	cOps = {a, ad};
+	tOps = {rho0, rho1};
+	// Ht = {sigmax};
+	Ht = {H0, Hising}
+	dt = 0.1; tmax = 10;
+	coeff = {2.3};
+	params = {{0.2, tmax}};
+
+	cout << H0 << endl;
+
+	fmat data;
+
+	data = me.evolveState(rho0, H0, cOps, tOps, Ht, params, pulse, dt, tmax, coeff, isSparse);
+
+	cout << data << endl;
+
+	return 0;
+}
